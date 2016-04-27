d781dd4a32f8f23d811340d6ae5240c323033c79
kickinbahk <kickinbahk@gmail.com>
2016-04-27 10:50:28 -0700

    add code testing functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,19 @@ var view = {
     messageArea.innerHTML = msg
   },
   displayHit: function (location) {
-    var cell document.getElementById(location)
+    var cell = document.getElementById(location)
     cell.setAttribute('class', 'hit')
   },
   displayMiss: function (location) {
-    var cell document.getElementById(location)
+    var cell = document.getElementById(location)
     cell.setAttribute('class', 'miss')
   }
 }
+
+view.displayMiss('00')
+view.displayHit('34')
+view.displayMiss('55')
+view.displayHit('12')
+view.displayMiss('25')
+view.displayHit('26')
+view.displayMessage('Hello?')
