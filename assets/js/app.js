1b94a6dbb4ed8884ff65624bd9c785a4951ae5bf
kickinbahk <kickinbahk@gmail.com>
2016-05-13 07:50:41 -0700

    add messages to view in methods

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,12 +30,17 @@ var model = {
 
       if (index >= 0) {
         ship.hits[index] = 'hit'
+        view.displayHit(guess)
+        view.displayMessage('HIT!')
         if (this.isSunk(ship)) {
+          view.displayMessage('You sank my battleship!')
           this.shipsSunk++
         }
         return true
       }
     }
+    view.displayMiss(guess)
+    view.displayMessage('You missed.')
     return false
   },
   isSunk: function (ship) {
