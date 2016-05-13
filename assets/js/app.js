abd7b4eabc0256566d74cfe02d2ec6e98815b5d3
kickinbahk <kickinbahk@gmail.com>
2016-05-13 08:17:18 -0700

    add parseguess method

--- assets/js/app.js
+++ assets/js/app.js
@@ -51,11 +51,34 @@ var model = {
     }
     return true
   }
-},
+}
 
 var controller = {
   guesses: 0,
   processGuess: function (guess) {
     // TODO: process the guess
+  },
+  parseGuess: function (guess) {
+    var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+    if (guess === null || guess.length !== 2) {
+      alert('Oops, please enter a letter and number on the board.')
+    } else {
+      var firstChar = guess.charAt(0)
+      var row = alphabet.indexOf(firstChar)
+      var column = guess.charAt(1)
+
+      if (isNaN(row) || isNaN(column)) {
+        alert(`Oops, that isn't on the board.`)
+      } else if (row < 0 ||
+        row >= model.boardSize ||
+        column < 0 ||
+        column >= 'model.boardSize') {
+          alert(`Oops, that's off the board.`)
+      } else {
+        return row + column
+      }
+    }
   }
+  return null
 }
