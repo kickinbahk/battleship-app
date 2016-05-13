307f3fda200d3893722650e5f008d6382ec9818d
kickinbahk <kickinbahk@gmail.com>
2016-05-13 07:34:17 -0700

    add ship model

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,10 +13,25 @@ var view = {
   }
 }
 
-view.displayMiss('00')
-view.displayHit('34')
-view.displayMiss('55')
-view.displayHit('12')
-view.displayMiss('25')
-view.displayHit('26')
-view.displayMessage('Hello?')
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipsSunk: 0,
+  shipLength: 3,
+  ships: [
+    {locations: ['06', '16', '26'], hits: ['', '', '']},
+    {locations: ['24', '34', '44'], hits: ['', '', '']},
+    {locations: ['10', '11', '12'], hits: ['', '', '']}
+  ],
+  fire: function (guess) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i]
+      var locations = ship.locations
+      var index = locations.indexOf(guess)
+
+      if (index >= 0) {
+        // hit 
+      }
+    }
+  }
+}
