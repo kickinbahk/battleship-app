3f23beb9aeb783bfd9f61724885e049084141be0
kickinbahk <kickinbahk@gmail.com>
2016-05-14 12:18:25 -0700

    add code to generate ship locations

--- assets/js/app.js
+++ assets/js/app.js
@@ -50,6 +50,16 @@ var model = {
       }
     }
     return true
+  },
+  generateShipLocations: function () {
+    var locations
+    for (var i = 0; i < this.numShips; i++) {
+      do  {
+        locations = this.generateShip()
+      } while (this.collision(locations)) {
+        this.ships[i].locations = locations
+      }
+    }
   }
 }
 
